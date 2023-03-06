54a58bcae8d463badb942f6b4ee0a07b27e82fd9
736327363@qq.com <736327346@qq.com>
2023-03-06 16:29:19 +0800

    Completion of project skeleton construction

--- backend/src/main/java/com/kob/backend/controller/BotInfoController.java
+++ backend/src/main/java/com/kob/backend/controller/BotInfoController.java
@@ -1,5 +1,6 @@
 package com.kob.backend.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,16 +15,10 @@ import java.util.*;
 public class BotInfoController {
 
     @RequestMapping("/getbotinfo")
-    public List<Map<String, String>> getBotInfo() {
-        List<Map<String, String>> list = new LinkedList<>();
+    public Map<String, String> getBotInfo() {
         Map<String, String> map1 = new HashMap<>();
-        map1.put("linchao", "1");
-        map1.put("steven", "2");
-        Map<String, String> map2 = new HashMap<>();
-        map2.put("steven", "3");
-        map2.put("linda", "4");
-        list.add(map1);
-        list.add(map2);
-        return list;
+        map1.put("name", "apple");
+        map1.put("rating", "2000");
+        return map1;
     }
 }
