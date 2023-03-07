6e14c68095ae52711dc974f5c99fdbd3b4fa3278
736327363@qq.com <736327346@qq.com>
2023-03-07 13:57:15 +0800

    navbar and route is ready

--- web/src/components/NavBar.vue
+++ web/src/components/NavBar.vue
@@ -1,20 +1,20 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-      <a class="navbar-brand" href="#">King Of Bots</a>
+      <router-link class="navbar-brand" :to="{name: 'home'}">King Of Bots</router-link>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarText">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link " aria-current="page" href="#">对战</a>
+            <router-link :class="route_name == 'pk_index' ? 'nav-link active' : 'nav-link'" :to="{name: 'pk_index'}">对战</router-link>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">对局列表</a>
+            <router-link :class="route_name == 'record_index' ? 'nav-link active' : 'nav-link'" :to="{name: 'record_index'}">对局列表</router-link>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">排行榜</a>
+            <router-link :class="route_name == 'ranklist_index' ? 'nav-link active' : 'nav-link'" :to="{name: 'ranklist_index'}">排行榜</router-link>
           </li>
         </ul>
         <ul class="navbar-nav">
@@ -23,7 +23,7 @@
               linchao
             </a>
             <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">我的Bot</a></li>
+              <li><router-link class="dropdown-item" :to="{name: 'user_bot_index'}">我的Bot</router-link></li>
               <li><hr class="dropdown-divider"></li>
               <li><a class="dropdown-item" href="#">退出</a></li>
             </ul>
@@ -36,7 +36,18 @@
 </template>
 
 <script>
+import { useRoute } from 'vue-router';
+import { computed } from "vue";
 
+export default {
+  setup() {
+    const route = useRoute();
+    let route_name = computed(() => route.name)
+    return {
+      route_name,
+    }
+  }
+}
 </script>
 
 <style>
--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,7 +1,45 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import NotFound from '../views/error/NotFound';
+import PkIndexView from '../views/pk/PkIndexView';
+import RanklistIndexView from '../views/ranklist/RanklistIndexView';
+import RecordlistIndexView from '../views/record/RecordlistIndexView';
+import UserBotIndexView from '../views/user/bot/UserBotIndexView';
 
 const routes = [
+  {
+    path: '/',
+    name: 'home',
+    redirect: '/pk/'
+  },
+  {
+    path: '/404/',
+    name: '404',
+    component: NotFound,
+  },
+  {
+    path: '/pk/',
+    name: 'pk_index',
+    component: PkIndexView,
+  },
+  {
+    path: '/record/',
+    name: 'record_index',
+    component: RecordlistIndexView,
+  },
+  {
+    path: '/rank/',
+    name: 'ranklist_index',
+    component: RanklistIndexView,
+  },
+  {
+    path: '/user/bot/',
+    name: 'user_bot_index',
+    component: UserBotIndexView,
+  },
+  {
+    path: '/:catchAll(.*)',
+    redirect: '/404/'
+  },
 
 ]
 
