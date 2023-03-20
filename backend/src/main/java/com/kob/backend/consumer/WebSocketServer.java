a63cf89e79455a442e2756b5e78cd2eb0b99fc37
736327363@qq.com <736327346@qq.com>
2023-03-20 21:01:31 +0800

    begin to websoket video on 19:56

---
+++ backend/src/main/java/com/kob/backend/consumer/WebSocketServer.java
@@ -0,0 +1,33 @@
+package com.kob.backend.consumer;
+
+import org.springframework.stereotype.Component;
+
+import javax.websocket.*;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+
+@Component
+@ServerEndpoint("/websocket/{token}")  // 注意不要以'/'结尾
+public class WebSocketServer {
+    @OnOpen
+    public void onOpen(Session session, @PathParam("token") String token) {
+        // 建立连接
+    }
+
+    @OnClose
+    public void onClose() {
+        // 关闭链接
+    }
+
+    @OnMessage
+    public void onMessage(String message, Session session) {
+        // 从Client接收消息
+    }
+
+    @OnError
+    public void onError(Session session, Throwable error) {
+        error.printStackTrace();
+    }
+}
+
+
